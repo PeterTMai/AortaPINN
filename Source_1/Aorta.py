b26d5d66236f3053e1775f991584b251e31b2215
Peter Mai <thanh.mai.17@ucl.ac.uk>
2021-03-03 21:00:03 +0000

    fixed save results script

--- Source_1/Aorta.py
+++ Source_1/Aorta.py
@@ -14,12 +14,12 @@ from utilities import neural_net, Navier_Stokes_3D, \
 
 class HFM(object):
     # notational conventions
-    # _tf: placeholders for input/output data and points used to regress the equations
+    # _tf: placeholders for input/output data andpoints used to regress the equations
     # _pred: output of neural network
     # _eqns: points used to regress the equations
     # _data: input-output data
     # _star: preditions
-    
+
     def __init__(self, t_data, x_data, y_data, z_data,
                        t_eqns, x_eqns, y_eqns, z_eqns,
                        u_data, v_data, w_data,
@@ -228,3 +228,67 @@ if __name__ == "__main__":
                 layers, batch_size, Rey = 1.0/0.0101822)
 
     model.train(total_time = 0.1, learning_rate=1e-3)
+
+    # Test Data
+    snap = np.array([100])
+    t_test = T_star[:,snap]
+    x_test = X_star[:,snap]
+    y_test = Y_star[:,snap]
+    z_test = Z_star[:,snap]
+
+    u_test = U_star[:,snap]
+    v_test = V_star[:,snap]
+    w_test = W_star[:,snap]
+    p_test = P_star[:,snap]
+
+    # Prediction
+    u_pred, v_pred, w_pred, p_pred = model.predict(t_test, x_test, y_test, z_test)
+
+    # Error
+    error_u = relative_error(u_pred, u_test)
+    error_v = relative_error(v_pred, v_test)
+    error_w = relative_error(w_pred, w_test)
+    error_p = relative_error(p_pred - np.mean(p_pred), p_test - np.mean(p_test))
+
+    print('Error u: %e' % (error_u))
+    print('Error v: %e' % (error_v))
+    print('Error w: %e' % (error_w))
+    print('Error p: %e' % (error_p))
+
+    ################# Save Data ###########################
+    U_pred = 0*U_star
+    V_pred = 0*V_star
+    W_pred = 0*W_star
+    P_pred = 0*P_star
+    for snap in range(0,t_star.shape[0]):
+        t_test = T_star[:,snap:snap+1]
+        x_test = X_star[:,snap:snap+1]
+        y_test = Y_star[:,snap:snap+1]
+        z_test = Z_star[:,snap:snap+1]
+
+        u_test = U_star[:,snap:snap+1]
+        v_test = V_star[:,snap:snap+1]
+        w_test = W_star[:,snap:snap+1]
+        p_test = P_star[:,snap:snap+1]
+
+        # Prediction
+        u_pred, v_pred, w_pred, p_pred = model.predict(t_test, x_test, y_test, z_test)
+
+        U_pred[:,snap:snap+1] = u_pred
+        V_pred[:,snap:snap+1] = v_pred
+        W_pred[:,snap:snap+1] = w_pred
+        P_pred[:,snap:snap+1] = p_pred
+
+        # Error
+        error_u = relative_error(u_pred, u_test)
+        error_v = relative_error(v_pred, v_test)
+        error_w = relative_error(w_pred, w_test)
+        error_p = relative_error(p_pred - np.mean(p_pred), p_test - np.mean(p_test))
+
+        print('Error u: %e' % (error_u))
+        print('Error v: %e' % (error_v))
+        print('Error w: %e' % (error_w))
+        print('Error p: %e' % (error_p))
+
+    scipy.io.savemat('../Results/Aneurysm3D_results_%s.mat' %(time.strftime('%d_%m_%Y')),
+                     {'U_pred':U_pred, 'V_pred':V_pred, 'W_pred':W_pred, 'P_pred':P_pred})
