1b4a945181d9e2938feff8f108305e08e3a01c1d
Peter Mai <thanh.mai.17@ucl.ac.uk>
2021-03-16 11:07:07 +0000

    Script to run Aorta_pressure.py

---
+++ Scripts/AortaPINN_pressure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -l tmem=15.9G
+#$ -l h_vmem=15.9G
+
+#$ -l h_rt=120:00:00
+
+# -S /bin/bash
+# -j y
+# -pe smp 4
+# -N AortaPINN_pressure
+# -wd /home/petermai/AortaPINN/AortaPINN
+
+hostname
+date
+echo ""
+source /share/apps/source_files/python/python-3.8.5.source
+cd /home/petermai/AortaPINN/AortaPINN/Source
+python3 Aorta_pressure.py > aorta_nondim_pressure
